0f9d2fced85a93273b7ec03eaa2ec1b327e846e6
Wouter Huysentruit <wouter.huysentruit@cognistreamer.be>
2016-09-02 10:47:05 +0200

    Add more unit-tests

--- tests/Owin.Security.CogniStreamer.Tests/Provider/CogniStreamerAuthenticatedContextTests.cs
+++ tests/Owin.Security.CogniStreamer.Tests/Provider/CogniStreamerAuthenticatedContextTests.cs
@@ -1,10 +1,45 @@
-﻿using NUnit.Framework;
+﻿using System;
+using Microsoft.Owin;
+using Moq;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Owin.Security.CogniStreamer.Provider;
 
 namespace Owin.Security.CogniStreamer.Tests.Provider
 {
     [TestFixture]
     public class CogniStreamerAuthenticatedContextTests
     {
-        // TODO
+        [Test]
+        public void CogniStreamerAuthenticatedContext_PassArgumentsToConstructor_ShouldSetProperties()
+        {
+            var owinContext = new Mock<IOwinContext>().Object;
+            var accessToken = Guid.NewGuid().ToString("N");
+            var user = JObject.Parse("{ a: 12345 }");
+            var context = new CogniStreamerAuthenticatedContext(owinContext, user, accessToken, "1400");
+            Assert.That(context.OwinContext, Is.EqualTo(owinContext));
+            Assert.That(context.User["a"].Value<int>(), Is.EqualTo(12345));
+            Assert.That(context.AccessToken, Is.EqualTo(accessToken));
+            Assert.That(context.ExpiresIn, Is.EqualTo(TimeSpan.FromSeconds(1400)));
+        }
+
+        [Test]
+        public void CogniStreamerAuthenticatedContext_PassUserObjectToConstructor_ShouldSetProperties()
+        {
+            var owinContext = new Mock<IOwinContext>().Object;
+            var user = JObject.Parse(@"{
+                id: 'dcec99ad-28e1-4194-a4e0-f22148963cc5',
+                username: 'cashj',
+                firstName: 'Johnny',
+                lastName: 'Cash',
+                email: 'cashj@walktheline.com'
+            }");
+            var context = new CogniStreamerAuthenticatedContext(owinContext, user, string.Empty, "3600");
+            Assert.That(context.Id, Is.EqualTo(new Guid("dcec99ad-28e1-4194-a4e0-f22148963cc5")));
+            Assert.That(context.Username, Is.EqualTo("cashj"));
+            Assert.That(context.FirstName, Is.EqualTo("Johnny"));
+            Assert.That(context.LastName, Is.EqualTo("Cash"));
+            Assert.That(context.Email, Is.EqualTo("cashj@walktheline.com"));
+        }
     }
 }
--- tests/Owin.Security.CogniStreamer.Tests/Provider/CogniStreamerReturnEndpointContextTests.cs
+++ tests/Owin.Security.CogniStreamer.Tests/Provider/CogniStreamerReturnEndpointContextTests.cs
@@ -1,10 +1,25 @@
-﻿using NUnit.Framework;
+﻿using System.Security.Claims;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Moq;
+using NUnit.Framework;
+using Owin.Security.CogniStreamer.Provider;
 
 namespace Owin.Security.CogniStreamer.Tests.Provider
 {
     [TestFixture]
     public class CogniStreamerReturnEndpointContextTests
     {
-        // TODO
+        [Test]
+        public void CogniStreamerReturnEndpointContext_PassArgumentsToConstructor_ShouldSetProperties()
+        {
+            var owinContext = new Mock<IOwinContext>().Object;
+            var identity = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Email, "some.email.address@some.server.com") });
+            var ticket = new AuthenticationTicket(identity, new AuthenticationProperties());
+            var context = new CogniStreamerReturnEndpointContext(owinContext, ticket);
+            Assert.That(context.OwinContext, Is.EqualTo(owinContext));
+            Assert.That(context.Identity, Is.EqualTo(identity));
+            Assert.That(context.Identity.HasClaim(ClaimTypes.Email, "some.email.address@some.server.com"));
+        }
     }
 }
