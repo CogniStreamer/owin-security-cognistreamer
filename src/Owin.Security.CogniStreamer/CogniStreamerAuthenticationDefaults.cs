59f309862d57be09f7a9cdb0dc6fe7d20c947eb0
Wouter Huysentruit <wouter.huysentruit@cognistreamer.be>
2016-09-02 16:33:24 +0200

    Rename Constants to CogniStreamerAuthenticationDefaults and make public

---
+++ src/Owin.Security.CogniStreamer/CogniStreamerAuthenticationDefaults.cs
@@ -0,0 +1,8 @@
+﻿namespace Owin.Security.CogniStreamer
+{
+    public static class CogniStreamerAuthenticationDefaults
+    {
+        public const string AuthenticationType = "CogniStreamer";
+        public const string CallbackPath = "/signin-cognistreamer";
+    }
+}
